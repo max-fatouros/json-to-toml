192104870b2de7421bd3a1b7b7fb6914cae47c74
Max Fatouros <max.fatouros@gmail.com>
2022-06-19 12:52:57 -0500

    Added if __name__ == "__main__" so that the traversal function can be used in other programs. Added parameter to choose name of keys that act as comments in the JSON file, instead of only allowing keys named "note" to contain comments. Added a function to open the output file and call the recursive traversal function.

--- main.py
+++ main.py
@@ -7,18 +7,24 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilenames
 
 
-def traverse(dictionary: dict, out_file, indent_spaces=4, depth=0,
-             comment_notes=True, comment_nulls=True, parent_keys=""):
+def traverse(dictionary: dict, out_file, indent_spaces=4, comment_key="", comment_nulls=True):
     """
     Recursively traverses dictionary and outputs a TOML file of its contents.
-    
+
     :param dictionary: Dictionary to be converted to TOML format.
-    :param out_file: TOML output file (Actual file, not just filename. Must open beforehand).
+    :param out_file: TOML output filename.
     :param indent_spaces: number of spaces for TOML indentation
+    :param comment_key: Any keys with this value with be replaced by a comment placed before the table in which it is contained.
+                    Leave empty to turn off this functionality
     :param comment_nulls: Replace null-types with TOML comments
-    :param comment_notes: Replace keys named "note" with comments placed before table taken from that keys value.
     """
 
+    with open(filename_out, "w", encoding='utf-8') as file:
+        traverse_helper(jDict, file, indent_spaces=4, comment=comment_key, comment_nulls=comment_nulls)
+
+
+def traverse_helper(dictionary: dict, out_file, indent_spaces=4, depth=0,
+                    comment="", comment_nulls=True, parent_keys=""):
     dicts = []
     keys = []
     for key, value in dictionary.items():
@@ -34,9 +40,9 @@ def traverse(dictionary: dict, out_file, indent_spaces=4, depth=0,
         if type(dictionary[key]) is dict:
             sub_dict = dictionary[key]
 
-            if comment_notes and 'note' in (key.lower() for key in sub_dict):
+            if comment != "" and comment in sub_dict:
                 out_file.write(depth * " " * indent_spaces)
-                out_file.write(f"# {sub_dict['Note']}\n")
+                out_file.write(f"# {sub_dict[comment]}\n")
                 del sub_dict['Note']
 
             # Prepends all parent key values to a sub-key string.
@@ -44,8 +50,8 @@ def traverse(dictionary: dict, out_file, indent_spaces=4, depth=0,
             out_file.write(depth * " " * indent_spaces)
             out_file.write(f"[{key}]\n")
 
-            traverse(sub_dict, out_file, indent_spaces=indent_spaces, depth=depth + 1,
-                     comment_notes=comment_notes, comment_nulls=comment_nulls, parent_keys=f"{key}.")
+            traverse_helper(sub_dict, out_file, indent_spaces=indent_spaces, depth=depth + 1,
+                            comment=comment, comment_nulls=comment_nulls, parent_keys=f"{key}.")
 
         else:
             value = dictionary[key]
@@ -74,34 +80,33 @@ def traverse(dictionary: dict, out_file, indent_spaces=4, depth=0,
                 out_file.write("\n")
 
 
-Tk().withdraw()  # Suppresses unnecessary window
-filenames_in = askopenfilenames()  # Opens window to choose JSON files, can choose multiple files.
+if __name__ == '__main__':
+    Tk().withdraw()  # Suppresses unnecessary window
+    filenames_in = askopenfilenames()  # Opens window to choose JSON files, can choose multiple files.
 
-for filename_in in filenames_in:
-    filename_out = os.path.splitext(filename_in)[0] + ".toml"
+    for filename_in in filenames_in:
+        filename_out = os.path.splitext(filename_in)[0] + ".toml"
 
-    with open(filename_in, encoding='utf-8') as file:
-        jDict = json.load(file)
+        with open(filename_in, encoding='utf-8') as file:
+            jDict = json.load(file)
 
-    with open(filename_out, "w", encoding='utf-8') as file:
-        # file.write(tomli.dumps(jDict))
-        traverse(jDict, file, indent_spaces=4)
+        traverse(jDict, filename_out, indent_spaces=4, comment_key="Note")
 
-    with open(filename_out, 'rb') as f:
-        # Just checking to make sure it can be read after conversion
-        try:
-            tDict = tomli.load(f)
-            print(f"Success reading {filename_out} as dictionary")
-        except tomli.TOMLDecodeError as e:
-            print(f"Error reading {filename_out} as dictionary")
-            print(e)
+        with open(filename_out, 'rb') as f:
+            # Just checking to make sure it can be read after conversion
+            try:
+                tDict = tomli.load(f)
+                print(f"Success reading {filename_out} as dictionary")
+            except tomli.TOMLDecodeError as e:
+                print(f"Error reading {filename_out} as dictionary")
+                print(e)
 
-# TODO: Compare dictionaries after processing.
-#  Need to implement comparison while taking into account that JSON supports null-types, while TOML does not.
-# if jDict == tDict:
-#     print("\u001b[32m", "Success!", "\u001b[0m")
-# else:
-#     print("\u001b[31m", "Failed to convert JSON to TOML", "\u001b[0m")
+    # TODO: Compare dictionaries after processing.
+    #  Need to implement comparison while taking into account that JSON supports null-types, while TOML does not.
+    # if jDict == tDict:
+    #     print("\u001b[32m", "Success!", "\u001b[0m")
+    # else:
+    #     print("\u001b[31m", "Failed to convert JSON to TOML", "\u001b[0m")
 
-# print(json.dumps(jDict, indent=4))
-# print(json.dumps(tDict, indent=4))
+    # print(json.dumps(jDict, indent=4))
+    # print(json.dumps(tDict, indent=4))
